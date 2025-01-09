cd9735863abd03949cc33136c7c46788dd668e1f
Varun  Israni <varunisrani@Varuns-MacBook-Pro.local>
2025-01-09 11:31:54 +0530

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
@@ -29,7 +28,6 @@ export default function RootLayout({
             {children}
             <Analytics />
             <SpeedInsights />
-            <Toaster />
           </div>
         </ThemeProvider>
       </body>
